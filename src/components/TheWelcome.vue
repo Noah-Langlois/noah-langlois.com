ce7fece6d38acf98446a8badd9daf5de8a513c41
noah.langlois <noahlanglois@hotmail.com>
2024-02-24 16:28:48 +0100

    Updated Home Page

--- src/components/TheWelcome.vue
+++ src/components/TheWelcome.vue
@@ -1,26 +1,15 @@
 <script setup></script>
 
 <template>
-  <div class="row p-5">
-    <div class="col-2">
-      <div>
-        <img class="profile-picture" src="../assets/Moi.jpg" />
-      </div>
-    </div>
-    <div class="col ps-5 pt-3">
-      <div class="card" style="width: 57%">
-        <div class="card-body">
-          <p>
-            <b>Bonjour ! &#128075;</b>
-            Je suis Noah LANGLOIS, étudiant ingénieur à l'<a href="https://www.ensma.fr/"
-              >ISAE-ENSMA</a
-            >
-            👨‍🎓 <br />
-            Bienvenue sur mon site internet qui regroupe mes infromations, divers projets et
-            docouments !
-          </p>
-        </div>
-      </div>
+  <div class="text-center" style="width: 57%">
+    <div class="">
+      <p>
+        <b>Bonjour ! &#128075;</b>
+        Je suis Noah LANGLOIS, étudiant ingénieur à l'<a href="https://www.ensma.fr/">ISAE-ENSMA</a>
+        👨‍🎓 <br />
+        Bienvenue sur mon site internet qui regroupe mes infromations, divers projets et docouments
+        !
+      </p>
     </div>
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,24 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import TheMe from '@/components/TheMe.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <div class="row">
+      <div class="col-5">
+        <div class="d-flex justify-content-center p-3">
+          <TheMe />
+        </div>
+        <div class="d-flex justify-content-center">
+          <TheWelcome />
+        </div>
+      </div>
+      <div class="col">
+        <div class="d-flex pt-5" style="height: 400px">
+          <div class="vr"></div>
+        </div>
+      </div>
+    </div>
   </main>
 </template>
