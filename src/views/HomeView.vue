8a8e9237b6ef87a0bc05b19fd5ab3f80556dfccf
noah.langlois <noahlanglois@hotmail.com>
2024-02-24 15:49:41 +0100

    Updated Links (Moved to the header)

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
-import TheLinks from '@/components/TheLinks.vue';
 import TheWelcome from '../components/TheWelcome.vue'
 </script>
 
 <template>
   <main>
     <TheWelcome />
-    <TheLinks/>
   </main>
 </template>
