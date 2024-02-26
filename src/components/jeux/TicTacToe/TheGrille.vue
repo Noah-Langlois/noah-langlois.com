b532b701da8358b094a5e812e62342b3545b3167
noah.langlois <noahlanglois@hotmail.com>
2024-02-26 15:37:07 +0100

    Updated TicTacToe

--- src/components/jeux/TicTacToe/TheGrille.vue
+++ src/components/jeux/TicTacToe/TheGrille.vue
@@ -1,6 +1,7 @@
 <script setup>
-import TheCroix from './TheCroix.vue'
-import TheRond from './TheRond.vue'
+import TheCase from './TheCase.vue'
+import { ref } from 'vue'
+const stateCases = ref(['rond', '', '', '', 'rond', '', '', '', 'rond'])
 </script>
 
 <template>
@@ -8,35 +9,35 @@ import TheRond from './TheRond.vue'
     <div class="grille">
       <div class="row">
         <div class="col border border-start-0 border-top-0">
-          <TheCroix />
+          <TheCase :stateCases="stateCases" :caseNumber="0" />
         </div>
         <div class="col border border-top-0">
-          <TheRond />
+          <TheCase :stateCases="stateCases" :caseNumber="1" />
         </div>
         <div class="col border border-top-0 border-end-0">
-          <TheCroix />
+          <TheCase :stateCases="stateCases" :caseNumber="2" />
         </div>
       </div>
       <div class="row">
         <div class="col border border-start-0">
-          <TheCroix />
+          <TheCase :stateCases="stateCases" :caseNumber="3" />
         </div>
         <div class="col border">
-          <TheCroix />
+          <TheCase :stateCases="stateCases" :caseNumber="4" />
         </div>
         <div class="col border border-end-0">
-          <TheCroix />
+          <TheCase :stateCases="stateCases" :caseNumber="5" />
         </div>
       </div>
       <div class="row">
         <div class="col border border-bottom-0 border-start-0">
-          <TheCroix />
+          <TheCase :stateCases="stateCases" :caseNumber="6" />
         </div>
         <div class="col border border-bottom-0">
-          <TheCroix />
+          <TheCase :stateCases="stateCases" :caseNumber="7" />
         </div>
         <div class="col border border-bottom-0 border-end-0">
-          <TheCroix />
+          <TheCase :stateCases="stateCases" :caseNumber="8" />
         </div>
       </div>
     </div>
