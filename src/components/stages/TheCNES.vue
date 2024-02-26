811bbaa11f840ea2d436dd70dd55c6a2db8db876
noah.langlois <noahlanglois@hotmail.com>
2024-02-26 14:25:21 +0100

    Updated Stages

--- src/components/stages/TheCNES.vue
+++ src/components/stages/TheCNES.vue
@@ -3,10 +3,17 @@
 <template>
   <div class="card">
     <div class="card-body">
-      <div class="card-title">
-        <h4>Projet de fin d'études</h4>
-        <h1>Interface Web pour la mécanique spatiale quotidienne</h1>
+      <div class="d-flex justify-content-between">
+        <div>
+          <h4>Projet de fin d'études (Avril-Septembre 2024)</h4>
+          <h1>Interface Web pour la mécanique spatiale quotidienne</h1>
+        </div>
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/a/a8/Logo_CNES_2017_triangulaire_bleu.png"
+          style="width: 9%"
+        />
       </div>
+      <div class="card-title"></div>
       <p>À venir</p>
     </div>
   </div>
