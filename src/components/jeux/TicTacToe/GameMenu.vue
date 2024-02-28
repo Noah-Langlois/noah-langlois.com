08c458f3d397ddbabb798a322fe96668b9222c78
noah.langlois <noahlanglois@hotmail.com>
2024-02-28 15:06:10 +0100

    Updated TicTacToe

---
+++ src/components/jeux/TicTacToe/GameMenu.vue
@@ -0,0 +1,19 @@
+<script setup>
+defineProps({
+  setGame: Function
+})
+</script>
+
+<template>
+  <div class="d-flex justify-content-center text-center">
+    <div>
+      <h1>Tic-Tac-Toe</h1>
+      <div class="pt-3">
+        <button class="btn btn-outline-dark" v-on:click="setGame('2P')">2 Joueur</button>
+      </div>
+      <div class="pt-3">
+        <button class="btn btn-outline-dark disabled">1 Joueur</button>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/TicTacToeView.vue
+++ src/views/TicTacToeView.vue
@@ -1,11 +1,22 @@
 <script setup>
 import TheGrille from '@/components/jeux/TicTacToe/TheGrille.vue'
+import GameMenu from '@/components/jeux/TicTacToe/GameMenu.vue'
+import { ref } from 'vue'
+
+const gameSelected = ref('')
+
+function setGame(pValue) {
+  gameSelected.value = pValue
+}
 </script>
 
 <template>
   <main>
-    <div class="pt-5">
-      <TheGrille />
+    <div class="pt-5" v-if="gameSelected === ''">
+      <GameMenu :setGame="setGame" />
+    </div>
+    <div class="pt-5" v-if="gameSelected === '2P'">
+      <TheGrille :setGame="setGame" />
     </div>
   </main>
 </template>
